b96d1b7e4af695815a35208c09d8a506ce45f0ee
Kita <nikitamarchenko54@gmail.com>
2026-02-14 19:56:29 +0800

    5002 fixed a port

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
       proxy: {
         "/api": {
-          target: "http://127.0.0.1:5002",
+          target: "http://127.0.0.1:5001",
           changeOrigin: true,
         },
       },
